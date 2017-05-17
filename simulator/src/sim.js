825268320211e5c1d8df50185f393ed90364d798
feroult <feroult@gmail.com>
2017-05-17 13:43:26 -0300

    sim

--- simulator/src/sim.js
+++ simulator/src/sim.js
@@ -3,39 +3,7 @@
 const Road = require('./road');
 const Vehicle = require('./vehicle');
 
-const argv = require('yargs')
-    .option('vehicles', {
-        alias: 'v',
-        describe: 'vehicles arriving in each interval',
-        default: 2
-    })
-    .option('interval', {
-        alias: 'i',
-        describe: 'interval (ms) to enter new vehicles',
-        default: 1000,
-    })
-    .option('min-velocity', {
-        alias: 'min',
-        describe: 'mininum target velocity for new vehicles',
-        default: 80
-    })
-    .option('max-velocity', {
-        alias: 'max',
-        describe: 'maximum target velocity for new vehicles',
-        default: 120
-    })
-    .options('sleep', {
-        alias: 's',
-        describe: 'vehicles interval (ms) time to emit events',
-        default: 1000,
-    })
-    .option('fast-forward', {
-        alias: 'fast',
-        describe: 'fast forward multiplier',
-        default: 1
-    })
-    .argv;
-
+const argv = require('./options');
 
 const stretches = [];
 for (let i = 0; i < 100; i++) {
