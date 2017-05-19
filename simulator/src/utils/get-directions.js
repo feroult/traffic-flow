cdadd02e13f3b93aecf1e0dba2a0664e6326361a
feroult <feroult@gmail.com>
2017-05-18 22:37:31 -0300

    bandeirantes setup

--- simulator/src/utils/get-directions.js
+++ simulator/src/utils/get-directions.js
@@ -8,23 +8,28 @@ const googleMaps = require('@google/maps').createClient({
 });
 
 const sq = x => x * x;
-const dist = (p1, p2) => Math.sqrt(sq(p1[0] - p2[0]) + sq(p1[1] - p2[1]));
+const dist = (p1, p2) => Math.sqrt(sq(p1[0] - p2[0]) + sq(p1[1] - p2[1])) * 100;
 
 const extractData = cb => {
     googleMaps.directions({
-        origin: 'Campinas',
-        destination: 'Sao Paulo',
+        // origin: 'Campinas',
+        // destination: 'Sao Paulo',
+        origin: '-22.997975, -47.103343',
+        destination: '-23.513111, -46.680075',
         mode: 'driving'
     }, (err, response) => {
         if (!err) {
             const points = response.json.routes[0].overview_polyline.points;
+
+            console.log('points', points);
+
             const decoded = polyline.decode(points);
             let sum = 0;
             const parsed = decoded.map((point, i) => {
                 sum += i == 0 ? 0 : dist(decoded[i - 1], point);
-                return { point, accSum : sum };
+                return {point, accSum: sum};
             });
-            cb(parsed);
+            cb({points: parsed});
         } else {
             console.log('err', err);
         }
@@ -40,13 +45,14 @@ const readDataFromFile = file => {
     return JSON.stringify(fs.readFileSync(file, 'utf-8'));
 };
 
-const maxDistance = data => data[data.length - 1].accSum;
+const maxDistance = ({points}) => points[points.length - 1].accSum;
 
-const interpolate = (data, x) => {
+const interpolate = ({points}, x) => {
     let p0, p1;
-    data.some((segment, i) => {
+
+    points.some((segment, i) => {
         if (segment.accSum > x) {
-            p0 = data[i - 1];
+            p0 = points[i - 1];
             p1 = segment;
             return true;
         }
@@ -55,9 +61,9 @@ const interpolate = (data, x) => {
     let totalDist = dist(p0.point, p1.point);
     let x0 = p0.point[0], x1 = p1.point[0], y0 = p0.point[1], y1 = p1.point[1];
     let fit = ls.solve([[x0, 1], [x1, 1]], [y0, y1]);
-    let fitfn = x => x*fit[0] + fit[1];
+    let fitfn = x => x * fit[0] + fit[1];
     let interpolatedX = x0 + diff * Math.cos(Math.atan2(y1 - y0, x1 - x0));
-    return [ interpolatedX, fitfn(interpolatedX) ];
+    return [interpolatedX, fitfn(interpolatedX)];
 };
 
-module.exports = { saveDataToFile, readDataFromFile, extractData, maxDistance, interpolate };
+module.exports = {saveDataToFile, readDataFromFile, extractData, maxDistance, interpolate};
