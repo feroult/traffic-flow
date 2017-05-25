8ea56113161d31c1e484cf65a0435e9a5b536353
feroult <feroult@gmail.com>
2017-05-25 19:10:30 -0300

    simulator control

--- simulator/src/sim-control.js
+++ simulator/src/sim-control.js
@@ -5,9 +5,9 @@ const argv = require('./options')(false);
 
 console.log('argv', argv);
 
-// const d = dnode.connect(argv.port);
-//
-// d.on('remote', function (remote) {
-//     remote.control(argv);
-//     process.exit(0);
-// });
+const d = dnode.connect(argv.port);
+
+d.on('remote', function (remote) {
+    remote.control(argv);
+    process.exit(0);
+});
