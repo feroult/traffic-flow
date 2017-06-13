8b21e3c5d4fcfc5e101ef774e21a65a6c012b729
feroult <feroult@gmail.com>
2017-06-12 22:45:54 -0300

    simulator

--- dataflow/deploy.sh
+++ dataflow/deploy.sh
@@ -1,6 +1,6 @@
-!/bin/sh
+#!/bin/sh
 
-mvn exec:java -Dexec.mainClass="io.github.feroult.dataflow.TrafficFlow"    \  
+mvn exec:java -Dexec.mainClass="io.github.feroult.dataflow.TrafficFlowPipeline"    \
                      -e -Dexec.args="--project=traffic-flow-app --sinkProject=traffic-flow-app  \
                      --stagingLocation=gs://traffic-flow-app --runner=DataflowPipelineRunner \
-                     --streaming=true --numWorkers=1 --zone=us-east1-c"
+                     --streaming=true --numWorkers=3 --maxNumWorkers=5 --zone=us-east1-c"
