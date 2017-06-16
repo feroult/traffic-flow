963d934bdd7123a4a89d08dea166b7b900804c36
feroult <feroult@gmail.com>
2017-06-16 11:23:52 -0300

    dataflow

--- dataflow/deploy.sh
+++ dataflow/deploy.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
-java -jar build/libs/dataflow-all-1.0.jar \
- --project=traffic-flow-app \
- --sinkProject=traffic-flow-app \
- --bigQueryTable=traffic-flow-app:demo.events \
- --zone=us-east1-c \
- --streaming \
- --stagingLocation=gs://traffic-flow-app/staging \
- --runner=DataflowRunner \
- --numWorkers=1 --maxNumWorkers=3
+
+read -d '' CMD << EOF
+    java -jar build/libs/dataflow-all-1.0.jar
+         --project=traffic-flow-app
+         --sinkProject=traffic-flow-app
+         --bigQueryTable=traffic-flow-app:demo.events
+         --zone=us-east1-c
+         --streaming
+         --stagingLocation=gs://traffic-flow-app/staging
+         --runner=DataflowRunner
+         --numWorkers=1 --maxNumWorkers=3
+EOF
+
+if [ -z "$1" ]; then
+    echo "Deploying new pipeline..."
+    eval $CMD
+else
+    echo "Updating existing pipeline: $1..."
+    eval $CMD --update --jobName=$1
+fi
+
+
